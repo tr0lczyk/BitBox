c4e3cd42858fa8a580a79d72d2941120b411c233
Mateusz <30321069+tr0lczyk@users.noreply.github.com>
2018-12-06 16:43:40 +0100

    databinding added to construction

--- app/src/main/java/com/olczyk/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/olczyk/android/beatbox/BeatBoxFragment.java
@@ -1,10 +1,26 @@
 package com.olczyk.android.beatbox;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.olczyk.android.beatbox.databinding.FragmentBeatBoxBinding;
 
 public class BeatBoxFragment extends Fragment {
 
     public static BeatBoxFragment newInstance(){
         return new BeatBoxFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater,R.layout.fragment_beat_box,container,false);
+        return binding.getRoot();
     }
 }
