9f92b755618d24519214d7766be0aaee00433078
tr0lczyk <matt.olczyk@gmail.com>
2018-12-09 23:59:08 +0100

    last changes

--- app/src/main/java/com/olczyk/android/beatbox/BeatBox.java
+++ app/src/main/java/com/olczyk/android/beatbox/BeatBox.java
@@ -26,6 +26,14 @@ public class BeatBox {
         loadSounds();
     }
 
+    public void play(Sound sound){
+        Integer soundId = sound.getSoundId();
+        if(soundId == null){
+            return;
+        }
+        soundPool.play(soundId,1.0f,1.0f,1,0,1.0f);
+    }
+
     private void loadSounds() {
         String[] soundNames;
         try {
