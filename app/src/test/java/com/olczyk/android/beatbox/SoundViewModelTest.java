57bb79d8a89add6fc80f453118d5ae7fb710858c
Mateusz <30321069+tr0lczyk@users.noreply.github.com>
2018-12-10 14:45:03 +0100

    start filling test class

---
+++ app/src/test/java/com/olczyk/android/beatbox/SoundViewModelTest.java
@@ -0,0 +1,13 @@
+package com.olczyk.android.beatbox;
+
+import org.junit.Before;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+public class SoundViewModelTest {
+    private BeatBox beatBox;
+    @Before
+    public void setUp() throws Exception {
+        beatBox = mock(BeatBox.class);
+    }
+}
